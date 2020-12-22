bf779b380ed1fb75f9afb32d0869d0372fb5305c
shahin <mohammadshahin237@gmail.com>
2020-12-21 20:53:00 -0800

    add procfile and gitignore

--- app.js
+++ app.js
@@ -174,7 +174,12 @@ app.post("/delete", function (req, res) {
 
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 300;
+}
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("server is running on port 3000");
+
+app.listen(port, function () {
+  console.log("server is started succesfully");
 });
